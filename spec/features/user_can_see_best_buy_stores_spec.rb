bebdc483fa9b1d003c1c02597247794eacf76bfb
mimilettd <mimilettd@gmail.com>
2017-11-01 09:22:53 -0600

    create feature test: user can see best buy stores

---
+++ spec/features/user_can_see_best_buy_stores_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+feature "User enters zip into search box" do
+  scenario "and sees stores within 25 miles of zip" do
+    visit "/"
+
+    fill_in "q", with: "80202"
+
+    click_on "Search"
+
+    expect(current_path).to eq("/search")
+
+    expect(page).to have_content("17 Total Stores")
+    expect(page).to have_css("store", :count => 10)
+    expect(page).to have_css(".longname")
+    expect(page).to have_css(".city")
+    expect(page).to have_css(".distance")
+    expect(page).to have_css(".phone")
+    expect(page).to have_css(".type")
+  end
+end
