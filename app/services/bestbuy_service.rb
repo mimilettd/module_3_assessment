76839456a5be4a7690924a54e75d3b4ffb2273a3
mimilettd <mimilettd@gmail.com>
2017-11-01 10:02:52 -0600

    make call

--- app/services/bestbuy_service.rb
+++ app/services/bestbuy_service.rb
@@ -1,9 +1,18 @@
 class BestbuyService
+  def initialize
+    @conn = Faraday.new(:url => 'https://api.bestbuy.com') do |faraday|
+    faraday.adapter  Faraday.default_adapter
+  end
+
+  end
   def fetch_by_zip(zip)
-    #https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=all&pageSize=10&apiKey=7a3heu7emrjz6qbsugmepbv6
+    response = conn.get("/v1/stores(area(zip,25))?format=json&show=all&pageSize=10&apiKey=#{ENV['bestbuy_api_key']}")
   end
 
   def self.fetch_by_zip(zip)
     new.fetch_by_zip(zip)
   end
+
+  private
+    attr_reader :conn
 end
