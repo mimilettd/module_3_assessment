5773ee6228aedf9ce83fa56428672c9249356b6f
mimilettd <mimilettd@gmail.com>
2017-11-01 11:11:46 -0600

    items api can send a collection of items and a given item

---
+++ spec/requests/api/v1/items/items_request_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe "Items API" do
+  context "Items Endpoint" do
+    it "can send a list of items" do
+      item = create_list(:item, 3).first
+
+      get "/api/v1/items"
+
+      expect(response).to be_success
+
+      items = JSON.parse(response.body)
+
+      expect(items.count).to eq(3)
+    end
+
+    it "can send an item" do
+      item = create(:item)
+
+      get "/api/v1/items/#{item.id}"
+
+      expect(response).to be_success
+
+      object = JSON.parse(response.body)
+
+      expect(object["name"]).to eq(item.name)
+      expect(object["description"]).to eq(item.description)
+      expect(object["image_url"]).to eq(item.image_url)
+    end
+  end
+end
